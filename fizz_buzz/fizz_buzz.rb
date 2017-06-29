0611e7d8c2815ecb6249015578386713fb7e5e15
Chris Donnelly <chris_j_donnelly@hotmail.com>
2017-06-29 11:27:12 +0100

    Returns Buzz

--- fizz_buzz/fizz_buzz.rb
+++ fizz_buzz/fizz_buzz.rb
@@ -1,6 +1,8 @@
 def fizz_buzz(i)
   if i % 3 == 0
     return "Fizz"
+  elsif i % 5 == 0
+    return "Buzz"
 end
 end
 
--- fizz_buzz/fizz_buzz_spec.rb
+++ fizz_buzz/fizz_buzz_spec.rb
@@ -7,4 +7,9 @@ class FizzBuzzSpec < MiniTest::Test
 def test_3_return_fizz()
   assert_equal("Fizz", fizz_buzz(3))
 end
+def test_5_return_buzz
+  assert_equal("Buzz", fizz_buzz(5))
+end
+
+
 end
