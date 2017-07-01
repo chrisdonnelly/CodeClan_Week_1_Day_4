9d06ff97709e37c7b7c88166375a8bc38c430648
Chris Donnelly <chris_j_donnelly@hotmail.com>
2017-07-01 18:39:43 +0100

    Initial Commit

--- fizz_buzz/fizz_buzz.rb
+++ fizz_buzz/fizz_buzz.rb
@@ -1,8 +1,12 @@
 def fizz_buzz(i)
-  if i % 3 == 0
+  if i % 3 == 0 && i % 5 == 0 
+    return "FizzBuzz"
+  elsif i % 3 == 0
     return "Fizz"
   elsif i % 5 == 0
     return "Buzz"
+  else
+    return "#{i}"
 end
 end
 
--- fizz_buzz/fizz_buzz_spec.rb
+++ fizz_buzz/fizz_buzz_spec.rb
@@ -10,6 +10,15 @@ end
 def test_5_return_buzz
   assert_equal("Buzz", fizz_buzz(5))
 end
+def test_6_return_fizz
+  assert_equal("Fizz", fizz_buzz(6))
+end
+def test_15_returns_fizzbuzz
+  assert_equal("FizzBuzz", fizz_buzz(15))
+end
 
+def test_37_returns_string_37
+  assert_equal("37", fizz_buzz(37))
+end
 
 end
